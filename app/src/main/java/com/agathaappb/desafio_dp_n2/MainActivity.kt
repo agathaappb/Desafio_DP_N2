d29f99705c304b8e6136aec4924cbc4ba4ebd06c
agathaappb <56099964+agathaappb@users.noreply.github.com>
2021-05-26 00:14:12 -0300

    Subindo um gif do funcionamento do app até agora

--- app/src/main/java/com/agathaappb/desafio_dp_n2/MainActivity.kt
+++ app/src/main/java/com/agathaappb/desafio_dp_n2/MainActivity.kt
@@ -2,50 +2,62 @@ package com.agathaappb.desafio_dp_n2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import android.widget.TextView
+import android.widget.LinearLayout
+import android.widget.ProgressBar
 import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.agathaappb.desafio_dp_n2.ServiceApiNoticias.GetModel
 import com.agathaappb.desafio_dp_n2.ServiceApiNoticias.GetService
 import com.agathaappb.desafio_dp_n2.ServiceApiNoticias.NewsAPIClient
+import com.agathaappb.desafio_dp_n2.View.NoticiasAdapter
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnNoticias:Button = findViewById(R.id.btnBuscarNoticias)
-        val txtNoticias: TextView = findViewById(R.id.txtNoticias)
+        val progress: ProgressBar = findViewById(R.id.progressBar)
+        progress.visibility = View.VISIBLE
 
-        btnNoticias.setOnClickListener {
+        val service = NewsAPIClient.createService(GetService::class.java)
+        val liveData = MutableLiveData<GetModel>()
+        val call: Call<GetModel> = service.getNoticia()
+
+        call.enqueue(object : Callback<GetModel> {
+            override fun onFailure(call: Call<GetModel>, t: Throwable) {
+                val resposta = t.message
+            }
+
+            override fun onResponse(call: Call<GetModel>, response: Response<GetModel>) {
+
+                val resp = response.body()
+                liveData.value = resp
+                val progress: ProgressBar = findViewById(R.id.progressBar)
+
+                val noticias = findViewById<View>(R.id.recyclerView) as RecyclerView
+                val adapter = liveData.value?.let { it1 -> NoticiasAdapter(it1.articles) }
+                noticias.adapter = adapter
+                noticias.layoutManager = LinearLayoutManager(this@MainActivity)
+
+                progress.visibility = View.GONE
 
 
-            val service = NewsAPIClient.createService(GetService::class.java)
-            val liveData = MutableLiveData<GetModel>()
-            val call : Call<GetModel> = service.getNoticia()
+            }
 
-            call.enqueue(object:Callback<GetModel>
-            {
-                override fun onResponse(
-                        call: Call<GetModel>,
-                        response: Response<GetModel>
-                ) {
-                    val resp = response.body()
+        })
 
-                }
 
-                override fun onFailure(call: Call<GetModel>, t: Throwable) {
-                    val resposta = t.message
-                }
-            })
-        }
 
 
     }
 
 
-
 }
+
--- app/src/main/java/com/agathaappb/desafio_dp_n2/View/NoticiasAdapter.kt
+++ app/src/main/java/com/agathaappb/desafio_dp_n2/View/NoticiasAdapter.kt
@@ -1,4 +1,36 @@
 package com.agathaappb.desafio_dp_n2.View
 
-class NoticiasAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.agathaappb.desafio_dp_n2.R
+import com.agathaappb.desafio_dp_n2.ServiceApiNoticias.GetArticles
+import com.agathaappb.desafio_dp_n2.ServiceApiNoticias.GetModel
+
+class NoticiasAdapter(private val getModel: List<GetArticles>): RecyclerView.Adapter<NoticiasAdapter.NoticiasViewHolder>(){
+    inner class NoticiasViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val noticiaTextView: TextView = itemView.findViewById(R.id.txt_descricao)
+        val imagemView :ImageView = itemView.findViewById(R.id.img_destaque)
+        val autorTextView : TextView = itemView.findViewById(R.id.txt_autor)
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoticiasViewHolder {
+        val cardView = LayoutInflater.from(parent.context).inflate(R.layout.layout_noticias,parent,false)
+        return NoticiasViewHolder(cardView)
+    }
+
+    override fun onBindViewHolder(holder: NoticiasViewHolder, position: Int) {
+        val itemPosition = getModel.get(position)
+            holder.noticiaTextView.text = itemPosition.description
+            holder.autorTextView.text = itemPosition.author
+    }
+
+    override fun getItemCount(): Int {
+        return getModel.size
+    }
+
 }
